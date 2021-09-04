e87ac3aba1df0c93e541dfd68507d5b767316d94
Raphael <veauville.raphael@gmail.com>
2021-09-04 16:46:56 +0200

    Fix links

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -18,7 +18,7 @@ module.exports = {
    *
    * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
    */
-  base: "/website/",
+  // base: "/website/",
   themeConfig: {
     repo: "",
     editLinks: false,
